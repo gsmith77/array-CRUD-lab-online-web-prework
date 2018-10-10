8a2406048a9133c3ff402d5c5dc0c94f9a34c321
grant smith <grantsmith1256@gmail.com>
2018-10-10 03:54:18 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,48 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  greetings = ["hello","hi","hola","hii"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(array,element)
+  greetings = ["hello","hi","hola","hii","arrays!"]
+  greetings << "arrays!"
 end
+
 
 def add_element_to_start_of_array(array, element)
-  
+  greetings = ["wow","hello","hi","hola","hii","arrays!"]
+  greetings.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  greetings = ["wow", "I", "am", "really", "learning","arrays!"]
+  greetings.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  greetings = ["wow", "I", "am", "really", "learning"]
+  greetings.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  greetings=["wow","I", "am", "really", "learning"]
+  greetings[1]
+end
+
+def retrieve_element_from_index(array, index_number)
+  greetings=["wow","I", "am", "really", "learning"]
+  greetings[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  greetings=["wow","I","am", "really", "learning"]
+  greetings.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+  greetings=["I", "am", "really", "learning","arrays!"]
+  greetings.last
 end
